f6d71f56022343dc15f2fef4652703677a9a5336
Pedro Salgado <steenzout@users.noreply.github.com>
2019-03-28 19:11:56 -0600

    improved variable naming and added docstring.

--- main.go
+++ main.go
@@ -4,33 +4,34 @@ import (
 	"fmt"
 )
 
-func ElapsedDays(arg1, arg2, arg3 int) {
+// ElapsedDays prints the number of days since the 1st of January of that year.
+func ElapsedDays(month, day, year int) {
 	elapsed := 0
 
-	if arg1 == 1 {
-		elapsed = arg2
-	} else if arg1 == 2 {
-		elapsed = 31 + arg2
-	} else if arg1 == 3 {
-		elapsed = 31 + 28 + arg2
-	} else if arg1 == 4 {
-		elapsed = 31 + 28 + 31 + arg2
-	} else if arg1 == 5 {
-		elapsed = 31 + 28 + 31 + 30 + arg2
-	} else if arg1 == 6 {
-		elapsed = 31 + 28 + 31 + 30 + 31 + arg2
-	} else if arg1 == 7 {
-		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + arg2
-	} else if arg1 == 8 {
-		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + 31 + arg2
-	} else if arg1 == 9 {
-		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + arg2
-	} else if arg1 == 10 {
-		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + arg2
-	} else if arg1 == 11 {
-		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + arg2
-	} else if arg1 == 12 {
-		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + arg2
+	if month == 1 {
+		elapsed = day
+	} else if month == 2 {
+		elapsed = 31 + day
+	} else if month == 3 {
+		elapsed = 31 + 28 + day
+	} else if month == 4 {
+		elapsed = 31 + 28 + 31 + day
+	} else if month == 5 {
+		elapsed = 31 + 28 + 31 + 30 + day
+	} else if month == 6 {
+		elapsed = 31 + 28 + 31 + 30 + 31 + day
+	} else if month == 7 {
+		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + day
+	} else if month == 8 {
+		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + 31 + day
+	} else if month == 9 {
+		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + day
+	} else if month == 10 {
+		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + day
+	} else if month == 11 {
+		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + day
+	} else if month == 12 {
+		elapsed = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + day
 	}
 
 	fmt.Println(elapsed)
